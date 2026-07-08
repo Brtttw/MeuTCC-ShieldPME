4573f570a120838dcc8d6a6d190ef1b21ef97f5c
Arthur <rm98133@estudante.fieb.edu.br>
2026-07-08 16:51:50 -0300

    Correção da pasta da API

---
+++ shieldpme-api/src/main/java/br/com/shieldpme/api/ShieldpmeApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.shieldpme.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShieldpmeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShieldpmeApiApplication.class, args);
+	}
+
+}
---
+++ shieldpme-api/src/test/java/br/com/shieldpme/api/ShieldpmeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.shieldpme.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShieldpmeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
